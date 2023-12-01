84ef6b71d2fa33d982876e3075392aa17969436a
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-12-01 09:27:01 +0100

    CalculatorTests

---
+++ tdd_231201/Classes/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_231201.Classes
+{
+    internal class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int[] GetArray(int value,  int count)
+        {
+            int[] array = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = value;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ tdd_231201/Tests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using tdd_231201.Classes;
+
+
+namespace tdd_231201.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+
+        [Test]
+        public void Add_ReturnsCorrectResult()
+        {
+            var expected = 5;
+            var actual = Calculator.Add(2,3);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Remove_ReturnsCorrectResult()
+        {
+            var result = Calculator.GetArray(3, 3);
+
+            Assert.That(result, Has.Exactly(3).EqualTo(3));
+        }
+    }
+}
