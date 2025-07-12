f464a9fce8029def3f7185534525d6a2862d198f
linus-rudbeck <linus.rudbeck@distansakademin.se>
2025-07-12 11:33:23 +0200

    Remove unused using directives from MusicManagerTests.cs

--- tdd_231201/Tests/MusicManagerTests.cs
+++ tdd_231201/Tests/MusicManagerTests.cs
@@ -1,10 +1,5 @@
 ﻿using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using tdd_231201.Musically;
 
 namespace tdd_231201.Tests
